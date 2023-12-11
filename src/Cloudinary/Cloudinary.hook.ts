6ce0560dbfcdd48436947b2c4586d936bca08508
Nan0ck <sgha10@hotmail.com>
2023-12-10 19:34:57 -0600

    [NO-ID] v1.0.0

---
+++ src/Cloudinary/Cloudinary.hook.ts
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { CloudinaryContextType } from "./Cloudinary.types";
+import { CloudinaryContext } from "./Clourdinary";
+
+export const useCloudinary = (): CloudinaryContextType => {
+  const cloudinary = useContext(CloudinaryContext);
+  if (!cloudinary) {
+    throw new Error("useCloudinary must be used within a CloudinaryProvider");
+  }
+  return cloudinary;
+};
